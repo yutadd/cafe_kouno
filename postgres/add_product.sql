f55c2c370d412035ba77a5dcd2362e43c53bd573
yutadd <spyk4m49@gmail.com>
2023-03-18 13:59:31 +0900

    add product description

--- postgres/add_product.sql
+++ postgres/add_product.sql
@@ -1,4 +1,4 @@
-insert into products(product_id,product_name,price,priceM,priceL,text,drink,category) values(0,'向野ブレンド',450,500,550,'驚くほどに、ふるふるとした食感と卵の味が感じられるカフェ河野の大人気商品台湾カステラ！ チョコ',true,'hot-coffee');
+insert into products(product_id,product_name,price,priceM,priceL,text,drink,category) values(0,'向野ブレンド',450,500,550,'甘い香りと柔らかな酸味が魅力のコロンビア豆。その中でも最高品質の豆を丁寧に焙煎し、力強いコクと甘みのある味わいにいたしました。最後の一滴まで心ゆくまでお楽しみくださいませ',true,'hot-coffee');
 insert into products(product_id,product_name,price,priceM,priceL,text,drink,category) values(1,'アメリカンコーヒー',490,540,590,'驚くほどに、ふるふるとした食感と卵の味が感じられるカフェ河野の大人気商品台湾カステラ！ チョコ',true,'hot-coffee');
 insert into products(product_id,product_name,price,priceM,priceL,text,drink,category) values(2,'カフェラテ',520,580,640,'驚くほどに、ふるふるとした食感と卵の味が感じられるカフェ河野の大人気商品台湾カステラ！ チョコ',true,'hot-coffee');
 insert into products(product_id,product_name,price,priceM,priceL,text,drink,category) values(3,'アイス向野ブレンド',450,500,550,'驚くほどに、ふるふるとした食感と卵の味が感じられるカフェ河野の大人気商品台湾カステラ！ チョコ',true,'ice-coffee');
@@ -8,11 +8,11 @@ insert into products(product_id,product_name,price,priceM,priceL,text,drink,cate
 insert into products(product_id,product_name,price,priceM,priceL,text,drink,category) values(7,'アイスティー',500,550,600,'驚くほどに、ふるふるとした食感と卵の味が感じられるカフェ河野の大人気商品台湾カステラ！ チョコ',true,'ice-other');
 insert into products(product_id,product_name,price,priceM,priceL,text,drink,category) values(8,'アイスロイヤルミルクティー',520,580,640,'驚くほどに、ふるふるとした食感と卵の味が感じられるカフェ河野の大人気商品台湾カステラ！ チョコ',true,'ice-other');
 insert into products(product_id,product_name,price,priceM,priceL,text,drink,category) values(9,'オレンジ100%ジュース',450,500,550,'驚くほどに、ふるふるとした食感と卵の味が感じられるカフェ河野の大人気商品台湾カステラ！ チョコ',true,'ice-other');
-insert into products(product_id,product_name,price,priceM,priceL,text,drink,category) values(10,'台湾カステラ(ノーマル)',1200,1200,1200,'驚くほどに、ふるふるとした食感と卵の味が感じられるカフェ河野の大人気商品台湾カステラ！ ノーマル',false,'food');
-insert into products(product_id,product_name,price,priceM,priceL,text,drink,category) values(11,'台湾カステラ(チョコ)',1400,1200,1200,'驚くほどに、ふるふるとした食感と卵の味が感じられるカフェ河野の大人気商品台湾カステラ！ チョコ',false,'food');
-insert into products(product_id,product_name,price,priceM,priceL,text,drink,category) values(12,'トースト',500,1200,1200,'驚くほどに、ふるふるとした食感と卵の味が感じられるカフェ河野の大人気商品台湾カステラ！ チョコ',false,'food');
-insert into products(product_id,product_name,price,priceM,priceL,text,drink,category) values(13,'フレンチトースト',550,1200,1200,'驚くほどに、ふるふるとした食感と卵の味が感じられるカフェ河野の大人気商品台湾カステラ！ チョコ',false,'food');
-insert into products(product_id,product_name,price,priceM,priceL,text,drink,category) values(14,'たまごサンド',620,1200,1200,'驚くほどに、ふるふるとした食感と卵の味が感じられるカフェ河野の大人気商品台湾カステラ！ チョコ',false,'food');
-insert into products(product_id,product_name,price,priceM,priceL,text,drink,category) values(15,'ハム＆たまごサンド',700,1200,1200,'驚くほどに、ふるふるとした食感と卵の味が感じられるカフェ河野の大人気商品台湾カステラ！ チョコ',false,'food');
-insert into products(product_id,product_name,price,priceM,priceL,text,drink,category) values(16,'カツサンド',850,1200,1200,'驚くほどに、ふるふるとした食感と卵の味が感じられるカフェ河野の大人気商品台湾カステラ！ チョコ',false,'food');
+insert into products(product_id,product_name,price,priceM,priceL,text,drink,category) values(10,'台湾カステラ(ノーマル)',1200,1200,1200,'雲のような、優しいおいしさ。驚くほどに、ふるふるとした食感。優しい甘さのカフェ向野の大人気商品台湾カステラ！',false,'food');
+insert into products(product_id,product_name,price,priceM,priceL,text,drink,category) values(11,'台湾カステラ(チョコ)',1400,1200,1200,'雲のような、優しいおいしさ。驚くほどに、ふるふるとした食感。ビターな甘さのカフェ向野の大人気商品台湾カステラ！',false,'food');
+insert into products(product_id,product_name,price,priceM,priceL,text,drink,category) values(12,'トースト',500,1200,1200,'素材にこだわった、トースト。パンは人気店の「MIYABI」、北海道の吉田牧場オリジナルバターをふんだんに使用しています。至極のトースト。',false,'food');
+insert into products(product_id,product_name,price,priceM,priceL,text,drink,category) values(13,'フレンチトースト',550,1200,1200,'シンプルなのに、美味しい。外はカリッと、内はアパレイユがじゅわっと。ボリューム満点の甘すぎないフレンチトースト',false,'food');
+insert into products(product_id,product_name,price,priceM,priceL,text,drink,category) values(14,'たまごサンド',620,1200,1200,'本格的で、美味しい。「MIYABI」のきめ細かくて柔らかい食パンと、北海道産の新鮮なたまごを使ったたまごサラダが、まろやかで懐かしい味わい。',false,'food');
+insert into products(product_id,product_name,price,priceM,priceL,text,drink,category) values(15,'ハム＆たまごサンド',700,1200,1200,'本格的で、美味しい。「MIYABI」のきめ細かくて柔らかい食パンと、北海道産の新鮮なたまごを使ったたまごサラダとハムが、まろやかで懐かしい味わい。',false,'food');
+insert into products(product_id,product_name,price,priceM,priceL,text,drink,category) values(16,'カツサンド',850,1200,1200,'飽きない。美味しさ。やわらかいヒレかつと、こんがり焼いたパン、甘めのソース、口の中に美味しさがいっぱいに広がります。',false,'food');
 
