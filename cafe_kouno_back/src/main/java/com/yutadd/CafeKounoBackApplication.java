97f59eb586034a1ae2d6869ab5b0703fb3a70a0d
yutadd <spyk4m49@gmail.com>
2023-03-20 21:07:48 +0900

    [update]addresses

--- cafe_kouno_back/src/main/java/com/yutadd/CafeKounoBackApplication.java
+++ cafe_kouno_back/src/main/java/com/yutadd/CafeKounoBackApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CafeKounoBackApplication {
 
 	//アクティベーションを行うためのページ（将来的にフロントエンド）のアドレスとポート
-public static String domain="yutadd.com";
-public static int port=3000;
+public static String domain="http://ws-hackathon2023-teams01.pencilsystems.site/";
+public static int port=80;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CafeKounoBackApplication.class, args);
